fe5ae4ed4fa76f289a4fa65d80caee96ed5c1412
moment <2742681975@qq.com>
2024-05-22 19:50:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
 const puppeteer = require('puppeteer')
 
+const fs = require('fs');
+const path = require('path');
+ 
+function getAllFilesInfo(dirPath) {
+    const itemsInfo = [];
+ 
+    function traverseDirectory(currentPath) {
+        const items = fs.readdirSync(currentPath);
+ 
+        for (const item of items) {
+            const itemPath = path.join(currentPath, item);
+            const stat = fs.statSync(itemPath);
+ 
+            if (stat.isFile() || stat.isDirectory()) {
+                itemsInfo.push({
+                    name: item,
+                    path: itemPath,
+                    size: stat.size,
+                    createdAt: stat.ctime,
+                    modifiedAt: stat.mtime,
+                    isDirectory: stat.isDirectory()
+                });
+            }
+ 
+            if (stat.isDirectory()) {
+                traverseDirectory(itemPath);
+            }
+        }
+    }
+ 
+    traverseDirectory(dirPath);
+    return itemsInfo;
+}
+ 
+const folderPath = '/opt/render/.cache/puppeteer/chrome-headless-shell/linux-125.0.6422.60';
+const itemsInfo = getAllFilesInfo(folderPath);
+console.log(itemsInfo);
+
 async function main() {
   const browser = await puppeteer.launch({
     executablePath: '/opt/render/.cache/puppeteer/chrome-headless-shell/linux-125.0.6422.60/chrome-headless-shell-win64/chrome-headless-shell',
@@ -19,4 +57,4 @@ async function main() {
   await browser.close()
 }
 
-main()
+// main()
