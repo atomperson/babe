1c8b7bd9c9ae6b9c9a4a68f4ea3e14ccc03e3f59
moment <2742681975@qq.com>
2024-05-22 18:36:31 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const puppeteer = require('puppeteer')
 
+import { join } from 'path'
+import puppeteer from 'puppeteer-core'
+import { PUPPETEER_REVISIONS } from 'puppeteer-core/lib/cjs/puppeteer/revisions.js'
+import { findChrome } from 'find-chrome-bin'
+
+async function init() {
+  let chromeInfo = await findChrome({
+    max: 95,
+    download: {
+      puppeteer,
+      path: join('.', 'chrome'),
+      revision: PUPPETEER_REVISIONS.chromium
+    }
+  })
+  console.log(chromeInfo)
+}
+
 async function main() {
   const browser = await puppeteer.launch({ executablePath: '/usr/bin/chromium-browser' })
   const page = await browser.newPage()
@@ -16,4 +33,4 @@ async function main() {
   await browser.close()
 }
 
-main()
+// main()
