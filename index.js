12eeddd892adab6b6dfbeb97e51175c88c14b5e8
moment <2742681975@qq.com>
2024-05-22 19:05:14 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const puppeteer = require('puppeteer')
 
 async function main() {
-  const browser = await puppeteer.launch({ executablePath: '/usr/bin/chromium-browser' })
+  const browser = await puppeteer.launch({
+    executablePath: '/opt/render/.cache/puppeteer/chrome-headless-shell/linux-125.0.6422.60/chrome-headless-shell-win64/chrome-headless-shell',
+  })
   const page = await browser.newPage()
+
   await page.goto('https://home-tvt1.onrender.com')
 
   const text = await page.waitForSelector('#text')
@@ -12,8 +15,8 @@ async function main() {
   })
 
   console.log(final)
-
+  
   await browser.close()
 }
 
-// main()
+main()
