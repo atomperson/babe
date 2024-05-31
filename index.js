a1c92e0a1719d43fdeb1c872c140ad53d556f911
moment <2742681975@qq.com>
2024-05-31 14:28:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const puppeteer = require('puppeteer')
 const fs = require('fs')
 const path = require('path')
 
-const port = process.env.PORT || 8000;
+const port = 8000;
 
 function getAllFilesInfo(dirPath) {
     const itemsInfo = [];
